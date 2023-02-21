618432a535a981c5e5487b3a447b967a9ade9f71
Stephen Malacaria <smala017@verizon.net>
2023-02-21 18:46:55 -0500

    fixing the links on hw6

---
+++ homework/02-14/dataLoading.js
@@ -0,0 +1,23 @@
+var i = 0;
+
+// NOTE: THE FLIGHTS CSV CANNOT BE INCLUDED BECAUSE IT EXCEEDS GITHUB'S FILE SIZE LIMIT
+// IT IS FOUND IN THE VizTech1 FOLDERR
+airportHeaders = "IATA_CODE,AIRPORT,CITY,STATE,COUNTRY,LATITUDE,LONGITUDE"
+csvStr = airportHeaders + "\n"
+d3.csv("../../FlightData/airports.csv").then(function(data) {
+    data.forEach(function(row) {
+        csvStr += row["IATA_CODE"] + "," + row["AIRPORT"] + "," + row["CITY"] + "," + row["STATE"] + "," + row["COUNTRY"] + "," + row["LATTITUDE"] + "," + row["LONGITUDE"] +"\n"
+    })
+    array = d3.csvParse(csvStr)
+    console.log(array)
+    // draw circles on each airport by long and lat
+
+})
+
+
+
+
+
+var svg = d3.select("#canvas")
+var circles = svg.selectAll("circle")
+
